af590739c747f3e55c2a347f7c08e0d218669f23
ocusler <ocusler@gmail.com>
2018-05-04 09:30:50 +0200

    slide23

--- src/test/java/com/drpicox/bowlingKata/BowlingGameTest.java
+++ src/test/java/com/drpicox/bowlingKata/BowlingGameTest.java
@@ -1,11 +1,19 @@
 package com.drpicox.bowlingKata;
 
 import junit.framework.TestCase;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class BowlingGameTest {
+
+    private Game g;
+
+    @Before
+    public void setUp() {
+        g = new Game();
+    }
 
     @Test
     public void testGutterGame() throws Exception {
@@ -13,5 +21,13 @@ public class BowlingGameTest {
         for (int i = 0; i < 20; i++)
             g.roll(0);
         assertEquals(0, g.score());
+    }
+
+    @Test
+    public void testAllOnes() throws Exception {
+        Game g = new Game();
+        for (int i = 0; i < 20; i++)
+            g.roll(1);
+        assertEquals(20, g.score());
     }
 }
--- src/test/java/com/drpicox/bowlingKata/Game.java
+++ src/test/java/com/drpicox/bowlingKata/Game.java
@@ -2,12 +2,13 @@ package com.drpicox.bowlingKata;
 
 public class Game {
 
+    private int score = 0;
 
     public void roll(int i) {
-
+        score += i;
     }
 
     public int score() {
-        return 0;
+        return score;
     }
 }
