d818a5c081019911f324b92ff8ee96343e88c5b0
ocusler <ocusler@gmail.com>
2018-05-04 10:22:30 +0200

    slide450

--- src/test/java/com/drpicox/bowlingKata/Game.java
+++ src/test/java/com/drpicox/bowlingKata/Game.java
@@ -14,8 +14,7 @@ public class Game {
         int score = 0;
         int frameIndex = 0;
         for (int frame = 0; frame < 10; frame++) {
-            if (rolls[frameIndex] == 10) // strike
-            {
+            if (isStrike(frameIndex)) {
                 score += 10 + strikeBonus(frameIndex);
                 frameIndex++;
             } else if (isSpare(frameIndex)) {
@@ -29,8 +28,12 @@ public class Game {
         return score;
     }
 
+    private boolean isStrike(int frameIndex) {
+        return rolls[frameIndex] == 10;
+    }
+
     private int sumOfBallsInFrame(int frameIndex) {
-        return rolls[frameIndex]+rolls[frameIndex+1];
+        return rolls[frameIndex] + rolls[frameIndex + 1];
     }
 
     private int spareBonus(int frameIndex) {
@@ -38,10 +41,10 @@ public class Game {
     }
 
     private int strikeBonus(int frameIndex) {
-        return rolls[frameIndex+1]+rolls[frameIndex+2];
+        return rolls[frameIndex + 1] + rolls[frameIndex + 2];
     }
 
     private boolean isSpare(int frameIndex) {
-        return rolls[frameIndex]+rolls[frameIndex+1] == 10;
+        return rolls[frameIndex] + rolls[frameIndex + 1] == 10;
     }
 }
