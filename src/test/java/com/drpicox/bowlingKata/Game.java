8bb06b3f92b908d92e5a8db9a9cd8b338a97da5e
ocusler <ocusler@gmail.com>
2018-05-04 09:45:31 +0200

    slide34

--- src/test/java/com/drpicox/bowlingKata/Game.java
+++ src/test/java/com/drpicox/bowlingKata/Game.java
@@ -3,9 +3,12 @@ package com.drpicox.bowlingKata;
 public class Game {
 
     private int score = 0;
+    private int rolls[] = new int[21];
+    private int currentRoll = 0;
 
     public void roll(int i) {
         score += i;
+        rolls[currentRoll++] = i;
     }
 
     public int score() {
