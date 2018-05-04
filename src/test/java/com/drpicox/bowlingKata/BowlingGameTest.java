1cb733ca7a7734e00a8c2558bf9ff4563d603b64
ocusler <ocusler@gmail.com>
2018-05-04 09:39:33 +0200

    slide25

--- src/test/java/com/drpicox/bowlingKata/BowlingGameTest.java
+++ src/test/java/com/drpicox/bowlingKata/BowlingGameTest.java
@@ -19,9 +19,7 @@ public class BowlingGameTest {
     public void testGutterGame() throws Exception {
         int n = 20;
         int pins = 0;
-
-        for (int i = 0; i < n; i++)
-            g.roll(pins);
+        rollMany(n, pins);
         assertEquals(0, g.score());
     }
 
@@ -31,5 +29,10 @@ public class BowlingGameTest {
         for (int i = 0; i < 20; i++)
             g.roll(1);
         assertEquals(20, g.score());
+    }
+
+    private void rollMany(int n, int pins) {
+        for (int i = 0; i < n; i++)
+            g.roll(pins);
     }
 }
